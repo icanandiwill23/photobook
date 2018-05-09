9296a7e706d66cdeb464c8f1c78410ae0c5e8474
Anthony <ajvergara2326@gmail.com>
2018-05-09 22:52:04 +0300

    Complated Icon Sprites for Large Hero

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,10 +1,40 @@
-const gulp = require("gulp"),
-      svgSprite = require("gulp-svg-sprite");
+const gulp      = require("gulp"),
+      svgSprite = require("gulp-svg-sprite"),
+      del       = require("del"),
+      rename    = require("gulp-rename");
 
-gulp.task("createSprite", function(){
+config = {
+  mode: {
+    css: {
+      sprite: "sprites.svg",
+      render: {
+        css: {
+          template: "./gulp/templates/sprites.css"
+        }
+      }
+    }
+  }
+}
+
+gulp.task("beginClean", function(){
+  return del(["./app/assets/images/sprites", "./app/temp/sprites"]);
+});
+
+gulp.task("createSprite", ["beginClean"], function(){
   return gulp.src("./app/assets/images/icons/**/*.svg")
     .pipe(svgSprite(config))
     .pipe(gulp.dest("./app/temp/sprites"));
 });
 
-gulp.tasl("icons", ["createSprite"]);
+gulp.task("copySpriteGraphic", ["createSprite"], function(){
+  return gulp.src("./app/temp/sprites/css/*.svg")
+    .pipe(gulp.dest("./app/assets/images/sprites"));
+});
+
+gulp.task("copySpriteCSS", ["createSprite"], function(){
+  return gulp.src("./app/temp/sprites/css/*.css")
+  .pipe(rename("_sprites.css"))
+  .pipe(gulp.dest("./app/assets/styles/modules"));
+});
+
+gulp.task("icons", ["beginClean", "createSprite", "copySpriteGraphic", "copySpriteCSS"]);
