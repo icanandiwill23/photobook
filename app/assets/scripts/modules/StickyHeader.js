9a46c1881373825c464002ded7be2a676ea80147
Anthony <ajvergara2326@gmail.com>
2018-05-20 21:36:54 +0300

    Completed Sticky Header Navigation - Smooth Scrolling Next

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -3,9 +3,13 @@ import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoi
 
 class StickyHeader{
   constructor(){
-    this.siteHeader = $(".header");
+    this.siteHeader        = $(".header");
     this.siteHeaderTrigger = $(".large-hero__subtitle");
     this.headerWaypoints();
+    this.pageSection       = $(".page-section");
+    this.pageSectionWaypoints();
+    this.navTags           = $(".primary-nav a");
+    this.clearNavWaypoint();
   }
 
   headerWaypoints(){
@@ -14,6 +18,46 @@ class StickyHeader{
       element: this.siteHeaderTrigger[0],
       handler: function(){
         that.siteHeader.toggleClass("header--is-lighter");
+      }
+    });
+  }
+
+  pageSectionWaypoints(){
+    var that = this;
+    this.pageSection.each(function(){
+      var currentPage = this;
+      new Waypoint({
+        element: currentPage,
+        handler: function(direction){
+          if(direction = "down"){
+            var matchinglink = currentPage.getAttribute("data-matching-link");
+            that.navTags.removeClass("is-current-link");
+            $(matchinglink).addClass("is-current-link");
+          }
+        },
+        offset: "65%"
+      });
+
+      new Waypoint({
+        element: currentPage,
+        handler: function(direction){
+          if(direction = "up"){
+            var matchinglink = currentPage.getAttribute("data-matching-link");
+            that.navTags.removeClass("is-current-link");
+            $(matchinglink).addClass("is-current-link");
+          }
+        },
+        offset: "-20%"
+      });
+    });
+  }
+
+  clearNavWaypoint(){
+    let that = this;
+    new Waypoint({
+      element: this.siteHeaderTrigger[0],
+      handler: function(){
+        that.navTags.removeClass("is-current-link");
       }
     });
   }
