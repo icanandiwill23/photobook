3c59a16601814ce2b620b8f13cb89518f830336e
Anthony <ajvergara2326@gmail.com>
2018-05-21 22:55:10 +0300

    Completed praparetion for GitHubs Pages

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -8,19 +8,19 @@ const gulp     = require("gulp"),
       htmlmin  = require("gulp-htmlmin"),
       browserSync  = require("browser-sync").create();
 
-gulp.task("previewDist", function(){
+gulp.task("previewDocs", function(){
   browserSync.init({
     server: {
-      baseDir: "dist"
+      baseDir: "docs"
     }
   });
 });
 
-gulp.task("deleteDist", ["icons"], function(){
-  return del("./dist");
+gulp.task("deleteDocs", ["icons"], function(){
+  return del("./docs");
 });
 
-gulp.task("generalFiles", ["deleteDist"], function(){
+gulp.task("generalFiles", ["deleteDocs"], function(){
   let pathsToCopy = [
     "./app/**/*",
     "!./app/assets/images/**/*",
@@ -30,10 +30,10 @@ gulp.task("generalFiles", ["deleteDist"], function(){
     "!./app/temp/**/*"
   ]
   return gulp.src(pathsToCopy)
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./docs"));
 });
 
-gulp.task("optimizeImages", ["deleteDist"], function(){
+gulp.task("optimizeImages", ["deleteDocs"], function(){
   return gulp.src(["./app/assets/images/**/*", "!./app/assets/images/icons/**/*"])
     .pipe(imagemin([
       imagemin.gifsicle({interlaced: true}),
@@ -46,15 +46,15 @@ gulp.task("optimizeImages", ["deleteDist"], function(){
         ]
       })
     ]))
-    .pipe(gulp.dest("./dist/assets/images"));
+    .pipe(gulp.dest("./docs/assets/images"));
 });
 
 gulp.task("dashed", ["optimizeImages"], function(){
   return gulp.src("./app/assets/images/icons/dashed.svg")
-    .pipe(gulp.dest("./dist/assets/images/icons"));
+    .pipe(gulp.dest("./docs/assets/images/icons"));
 });
 
-gulp.task("useminTrigger", ["deleteDist"], function(){
+gulp.task("useminTrigger", ["deleteDocs"], function(){
   gulp.start("usemin");
 });
 
@@ -65,7 +65,7 @@ gulp.task("usemin", ["styles", "scripts"], function(){
       html: [function(){ return htmlmin({collapseWhitespace: true}) }],
       js: [function(){ return uglify() }, function(){ return rev()  }]
     }))
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./docs"));
 });
 
-gulp.task("build", ["deleteDist", "generalFiles", "optimizeImages", "dashed", "useminTrigger"]);
+gulp.task("build", ["deleteDocs", "generalFiles", "optimizeImages", "dashed", "useminTrigger"]);
